9947de3fef05205705a42f6a5d96ec3f38fd7ccb
Dariusz Kakietek <Dariusz.Kakietek@de.obi.net>
2023-12-07 13:26:17 +0100

    init commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+### REST API - Tworzenie notatek
+
+- Projekt zaliczeniowy z przedmiotu "Tworzenie backendu do aplikacji webowych"
+- Numer albumu: 76768
---
+++ server.js
@@ -0,0 +1,36 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./config/connectToDb");
+const notesController = require("./controllers/notesController");
+const usersController = require("./controllers/usersController");
+const requireAuth = require("./middleware/requireAuth");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
+
+connectToDb();
+
+app.get("/", (req, res) => {
+  res.json({ hello: "world" });
+});
+
+app.post("/signup", usersController.signup);
+app.post("/login", usersController.login);
+app.get("/logout", usersController.logout);
+app.get("/check-auth", requireAuth, usersController.checkAuth);
+
+app.get("/notes", notesController.fetchNotes);
+app.get("/notes/:id", notesController.fetchNote);
+app.post("/notes", notesController.createNote);
+app.put("/notes/:id", notesController.updateNote);
+app.delete("/notes/:id", notesController.deleteNote);
+
+app.listen(process.env.PORT || 3000);
